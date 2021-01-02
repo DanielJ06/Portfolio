9768f9bf5a137173d85ab6c26ee65e2aba0ba14b
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-01-02 12:54:37 -0300

    Styled-components instead of tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import App from 'next/app'
-import '../css/styles.css'
+import { GlobalStyle } from '../styles/global'
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
     return (
       <React.Fragment>
+        <GlobalStyle />
         <style jsx global>
-        {`
-            @import url('https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;700&display=swap');
-        `}
+          {`@import url(https://fonts.googleapis.com/css2?family=Inter:wght@400;500;800&display=swap);`}
         </style>
         <Component {...pageProps} />
       </React.Fragment>
--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    theme: {
-        fontFamily: {
-            sans: ['"Titillium Web"']
-        },
-    }
-}
